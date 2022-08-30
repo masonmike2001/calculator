c9201b5c23f4c0c35c41c4673d91e09b7fa5628e
masonmike2001 <masonmike2001@gmail.com>
2022-08-30 12:56:17 -0400

    added all extra features except for decimal rounding and keyboard integration

--- js-file.js
+++ js-file.js
@@ -4,7 +4,10 @@ let calcQueue = new Array();
 let operandQueue = new Array();
 let queueSize = 0;
 let inputDigits = 1;
-let isFirstInput = true;
+let isEquationComplete = false;
+let isDecimalMode = false;
+let decimalAmount = 0;
+
 
 
 //creates the DOM vars and event listeners that are needed
@@ -24,15 +27,17 @@ leftButtons.forEach(button => {
 mainButtons.forEach(button => {
     button.addEventListener("click", updateInput);
 });
-clearButton.addEventListener("click", updateInput);
+mainButtons.forEach(button => {
+    button.addEventListener("keydown", updateInput);
+});
+clearButton.addEventListener("click", clearCalculator);
 equalButton.addEventListener("click", addOperator);
 
-
-
-
-//main functions
+//main functions------------------------------------------------------------------------------------------------------------------------------------------------
 function addOperator (e) //after an operator key is pressed, saves the finished number and operator into the calcQueue array
 {
+    isDecimalMode = false;
+    decimalAmount = 0;
     if (calcQueue[queueSize] == undefined)
     //returns if user presses operand before number entry
     {
@@ -41,17 +46,14 @@ function addOperator (e) //after an operator key is pressed, saves the finished 
     if (e.target == leftButtons[0])
     {
         operandQueue[queueSize] = "+";
-        
     }
     else if (e.target == leftButtons[1])
     {
         operandQueue[queueSize] = "-";
-        
     }
     else if (e.target == leftButtons[2])
     {
         operandQueue[queueSize] = "*";
-        
     }
     else if (e.target == leftButtons[3])
     {
@@ -61,6 +63,7 @@ function addOperator (e) //after an operator key is pressed, saves the finished 
     {
         displayHistory.innerText += ' ' +  calcQueue[queueSize] + ' ';
         display.innerText = operate(calcQueue, operandQueue);
+        isEquationComplete = true;
         return 0;
     }
     displayHistory.innerText += ' ' +  calcQueue[queueSize] + ' ' + operandQueue[queueSize] + ' ';
@@ -85,7 +88,6 @@ function operate (nums, operators) //chooses an operator and executes it, return
         operators.splice(index, 1);
         index = operators.indexOf('/');
     }
-
     while(operators.indexOf('+') != -1)
     {
         index = operators.indexOf('+');
@@ -93,7 +95,6 @@ function operate (nums, operators) //chooses an operator and executes it, return
         operators.splice(index, 1);
         index = operators.indexOf('+');
     }
-
     while(operators.indexOf('-') != -1)
     {
         index = operators.indexOf('-');
@@ -106,11 +107,11 @@ function operate (nums, operators) //chooses an operator and executes it, return
 
 function updateInput (e) //takes the key input (tens and ones digit) and updates the total -> puts it on display
 {
-
+    if (isEquationComplete) clearCalculator();
     switch(e.target)
         //checks for which number key is pressed (if . or del)
     {
-        case(mainButtons[0]):
+        case(mainButtons[0] || e.keyCode == 49):
             input = 1;
             break;
         case(mainButtons[1]):
@@ -139,15 +140,36 @@ function updateInput (e) //takes the key input (tens and ones digit) and updates
             break;   
         case(mainButtons[9]):
             //decimal    
-
+            input = 0.0;
+            isDecimalMode = true;
             break;
         case(mainButtons[10]):
             input = 0;
             break;
         case(mainButtons[11]):
             //backspace
+            displayValue -= (displayValue % 10);
+            displayValue /= 10;
+            display.innerText = displayValue;
+            if (displayValue == undefined) displayValue = 0;
+            return 0;
+    }
 
-            break;   
+    if (isDecimalMode == true)
+    {
+        let power = 1.0;
+        for (i = 0; i < decimalAmount; i++)
+        {
+            power *= 10;
+        }
+        displayValue += input / power;
+
+//make it so thst it works for decimals
+    inputDigits++;
+    calcQueue[queueSize] = displayValue / 1.0;
+    display.innerText = calcQueue[queueSize];
+        decimalAmount++;
+        return 0;
     }
     if (inputDigits != 1)
     //if not the first number multiplies displayValue by ten to correct digit
@@ -161,11 +183,22 @@ function updateInput (e) //takes the key input (tens and ones digit) and updates
         display.innerText = displayValue;
     }
     inputDigits++;
-    calcQueue[queueSize] = displayValue;
+    calcQueue[queueSize] = displayValue / 1.0;
     display.innerText = calcQueue[queueSize];
-    input = 0;
-
 }
+
+function clearCalculator()
+{
+    displayValue = 0;
+    display.innerText = displayValue;
+    displayHistory.innerText = '';
+    calcQueue = new Array();
+    operandQueue = new Array();
+    queueSize = 0;
+    inputDigits = 1
+    isEquationComplete = false;
+}
+
 //operator functions
 function addInputs (a, b) {
     return a + b;
@@ -182,19 +215,3 @@ function multiplyInputs (a, b) {
 function divideInputs (a, b) {
     return a / b;
 }
-
-
-
-
-/* TEST CASES:
-
-checks for function operation for operators:
-console.log(addInputs(100,10));
-console.log(subtractInputs(100,10));
-console.log(multiplyInputs(100,10));
-console.log(divideInputs(100,10));
-
-
-
-
-*/
